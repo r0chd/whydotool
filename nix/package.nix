54df093136284a744b208d13341c8e16c69bcc01
r0chd <100892812+unixpariah@users.noreply.github.com>
2026-01-04 16:10:12 +0100

    some nix package stuff

--- nix/package.nix
+++ nix/package.nix
@@ -46,11 +46,12 @@ rustPlatform.buildRustPackage.override { stdenv = clangStdenv; } {
 
   LIBCLANG_PATH = "${llvmPackages_20.libclang.lib}/lib";
 
+  buildNoDefaultFeatures = true;
   cargoFeatures = lib.optionals portals [ "portals" ];
 
   meta = {
     description = "Wayland-native command-line automation tool.";
-    homepage = "https://github.com/r0chd/whydotool";
+    homepage = "https://forgejo.r0chd.pl/r0chd/whydotool";
     license = lib.licenses.mit;
     maintainers = builtins.attrValues { inherit (lib.maintainers) r0chd; };
     platforms = lib.platforms.linux;
